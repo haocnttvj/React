4d0add6876896056b15df0d6ccd0a4cdeb0b7103
HaoPro <haopka.cnttvj@gmail.com>
2023-02-27 21:16:19 +0700

    new file:   Bai5/Thuc_hanh1

---
+++ Bai5/Thuc_hanh1/index.js
@@ -0,0 +1,16 @@
+let a = "bien thu nhat";
+let b = "bien thu hai";
+
+console.log("Hoan hoi 2 bien");
+
+console.log("Bien thu nhat:", a);
+console.log("Bien thu hai:", b);
+
+let tmp = a;
+a = b;
+b = tmp;
+
+console.log("Sau hoan doi");
+
+console.log("Bien thu nhat:", a);
+console.log("Bien thu hai:", b);
