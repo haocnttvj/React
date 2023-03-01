2aeea19c59fb0fdca8f83073ae53fafa566a90d3
HaoPro <haopka.cnttvj@gmail.com>
2023-03-01 11:55:34 +0700

    new

---
+++ Bai5/Baitap1/index.js
@@ -0,0 +1,21 @@
+let array = [6,9,8,9,7,10]
+let averageGrade = 0;
+
+function findAverageGrade(array){
+    if(!array || !array.length) return "Lỗi!";
+
+    for (let index = 0;index<array.length;index++){
+        averageGrade += array[index];
+    }
+    averageGrade = (averageGrade/array.length)*10;
+}
+function grade(averageGrade){
+    if(0<=averageGrade && averageGrade <=70) return "Grade: D";
+    else if(71<= averageGrade && averageGrade <=79) return "Grade: C";
+    else if(81<= averageGrade && averageGrade <=89) return "Grade: B";
+    else if(91<= averageGrade && averageGrade <=100) return "Grade: A";
+}
+
+findAverageGrade(array);
+console.log(averageGrade);
+console.log(grade(averageGrade));
